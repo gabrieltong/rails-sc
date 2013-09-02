267dfbc9abc96205726ad7e62ab29bef3173b7b7
tongyang <tongyangmath@gmail.com>
2013-09-02 23:42:40 +0800

    init commit

---
+++ app/models/person.rb
@@ -0,0 +1,3 @@
+class Person < ActiveRecord::Base
+  attr_accessible :alumnus, :exam_4, :exam_6, :exam_qimo, :exam_toefl, :finished, :guide, :guide_vip, :home_101, :home_51, :home_summer, :logic, :phone, :qq, :state, :username
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSc::Application.config.secret_token = '9601d96b05c8fcae741cd9b39fd5b7103ea3c2877c585f791b190b4f835b9f089b8318202f5f1fc0ebcacac47ecb67c230c15999bab359e1c2651fedcbd8b742'
