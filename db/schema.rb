fcd1d9c5377f50bb6cb9fe747bf102c03ccf02ee
gabrieltong <tongyangmath@gmail.com>
2013-09-04 17:09:41 +0800

    添加性别到用户

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130904081101) do
+ActiveRecord::Schema.define(:version => 20130904084622) do
 
   create_table "klass_joins", :force => true do |t|
     t.integer  "klass_id"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(:version => 20130904081101) do
     t.integer  "major_id"
     t.integer  "grade"
     t.boolean  "signup_openclass"
+    t.string   "sex"
   end
 
   create_table "planitems", :force => true do |t|
